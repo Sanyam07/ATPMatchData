754ae43f8ee004cd0703b40e93ec13d66f607ed1
bschoney <bschoney@utexas.edu>
2019-08-03 19:45:05 -0500

    pandas install

--- scraping_scripts/tennisdata.py
+++ scraping_scripts/tennisdata.py
@@ -1,8 +1,11 @@
 import datadotworld as dw
+from app import db
+
+session = db.session()
 
 lds = dw.load_dataset('tylerudite/atp-match-data')
 pandas_matches = lds.dataframes['atp_matches_combined']
 pandas_players = lds.dataframes['atp_players']
 
 if __name__ == '__main__':
-    pass
+    
